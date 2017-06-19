8c5c928c90de829536eccce3df2c1b23bc1dc7e0
zya-performance <sriram.durbha@zyamusic.com>
2017-06-19 15:24:44 -0700

    link statically, additional debug prints

--- process.cpp
+++ process.cpp
@@ -1,16 +1,29 @@
 #include "process.hpp"
+#include <iostream>
 
-TPL::Process::Process(const string_type &command, const string_type &path,
-                 std::function<void(const char* bytes, size_t n)> read_stdout,
-                 std::function<void(const char* bytes, size_t n)> read_stderr,
-                 bool open_stdin, size_t buffer_size):
-                 closed(true), read_stdout(read_stdout), read_stderr(read_stderr), open_stdin(open_stdin), buffer_size(buffer_size) {
-  open(command, path);
-  async_read();
+TPL::Process::Process(
+    const string_type &command,
+    const string_type &path,
+    std::function<void(const char* bytes, size_t n)> read_stdout,
+    std::function<void(const char* bytes, size_t n)> read_stderr,
+    bool open_stdin,
+    size_t buffer_size
+) :
+    closed(true),
+    read_stdout(read_stdout),
+    read_stderr(read_stderr),
+    open_stdin(open_stdin),
+    buffer_size(buffer_size)
+{
+    std::cout << "TPL Constructor opening with command : " << command << std::endl;
+    open(command, path);
+    std::cout << "TPL Constructor launching async_read : " << command << std::endl;
+    async_read();
 }
 
 TPL::Process::~Process() {
-  close_fds();
+    std::cout << "TPL  Destructor closing fds          : " << std::endl;
+    close_fds();
 }
 
 TPL::Process::id_type TPL::Process::get_id() {
